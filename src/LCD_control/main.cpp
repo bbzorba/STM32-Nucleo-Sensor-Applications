5a9a9247b1c4aa9a60775967fa81787a8074b26a
bbzorba <bbzorba@hotmail.com>
2024-08-10 13:35:46 +0200

    added LCD control

---
+++ src/LCD_control/main.cpp
@@ -0,0 +1,55 @@
+#include "lcd.h"
+
+int main(void)
+{
+	HAL_Init();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	
+	pinStruct_t rs = {GPIOE,GPIO_PIN_9};
+	pinStruct_t en = {GPIOE,GPIO_PIN_11};
+	pinStruct_t d4 = {GPIOE,GPIO_PIN_14};
+	pinStruct_t d5 = {GPIOE,GPIO_PIN_13};
+	pinStruct_t d6 = {GPIOE,GPIO_PIN_8};
+	pinStruct_t d7 = {GPIOE,GPIO_PIN_7};
+	
+	static LCD lcd(rs,en,d4,d5,d6,d7);
+	
+	//Example 1
+//	lcd.Print('A');
+//	lcd.SetCursor(2,1);
+//	lcd.Print("How are you?");
+	
+	//Example 2
+//	uint8_t x = 10;
+//	uint16_t y = 5600;
+//	uint32_t z = 1234560;
+//	
+//	lcd.Print(x);
+//	lcd.SetCursor(1,0);
+//	lcd.Print(y);
+//	lcd.SetCursor(2,0);
+//	lcd.Print(z);
+	
+	//Example 3
+	lcd.Print("Hello world");
+	HAL_Delay(1000);
+	lcd.Clear();
+	HAL_Delay(1000);
+	lcd.Print("Lovely goal");
+	HAL_Delay(1000);
+	lcd.Clear();
+	HAL_Delay(1000);
+	uint8_t var = 7;
+	lcd.Print(var);
+	
+	while(1)
+	{
+	}
+	
+}
+
+extern "C" void SysTick_Handler(void)
+{
+	HAL_IncTick();
+}
+
